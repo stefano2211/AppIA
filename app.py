534966883ce7abe9a3581ada8ea9e7ab77a21d73
stefano2211 <stefano.andres2004@gmail.com>
2025-03-03 08:40:37 -0400

    `Added comment "hOLA" to API_URL variable`

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from datetime import datetime
 
-# Configuración de la API FastAPI
+# hOLA
 API_URL = "http://127.0.0.1:8000"  # Cambia esto si tu API está en otro lugar
 
 # Función para manejar el login
